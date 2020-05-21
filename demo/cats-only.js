daa27e537578f33eca20b10ba71ed4daca955463
Ramyavijaykumar <ramyasumi05@gmail.com>
2020-05-21 09:08:01 +0530

    added my project

---
+++ demo/cats-only.js
@@ -0,0 +1,43 @@
+
+import '@polymer/polymer/polymer-legacy.js';
+
+import {IronValidatableBehavior} from '@polymer/iron-validatable-behavior/iron-validatable-behavior.js';
+import {Polymer} from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import {html} from '@polymer/polymer/lib/utils/html-tag.js';
+Polymer({
+  _template: html`
+    <style>
+      :host {
+        display: inline-block;
+      }
+      input {
+        width: 250px;
+      }
+      :host([invalid]) input {
+        border-color: red;
+      }
+    </style>
+    <input value="{{value}}" id="input" placeholder="Type 'cats' to prove you like cats">
+`,
+
+  is: 'cats-only',
+
+  properties: {
+    value: {type: String},
+
+    name: {type: String}
+  },
+
+  behaviors: [IronValidatableBehavior],
+  listeners: {'input': '_onInput'},
+
+  _onInput: function() {
+    this.value = this.$.input.value;
+  },
+
+  // Overidden from Polymer.IronValidatableBehavior. Will set the `invalid`
+  // attribute automatically, which should be used for styling.
+  _getValidity: function() {
+    return this.value === 'cats';
+  }
+});
